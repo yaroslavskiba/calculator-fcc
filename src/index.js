c34d02975a0188235ad70ae23f7b24c07718e116
yaroslav skiba <109871666+yaroslavskiba@users.noreply.github.com>
2023-02-08 16:49:11 +0300

    style pt.1

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
 function App() {
+  const [display, setDisplay] = useState('0');
+  const buttons = ['AC', '/', 'x', '7', '8', '9', '-', '6', '5', '4', '+', '3', '2', '1', '0', '.', '='];
   return (
-    <div className='container'>
+    <>
       <div className='calculator'>
         <div className='display'>
           {/* current state */}
+          0
         </div>
-        {/* buttons */}
+        <div className='buttons-container'>
+          {buttons.map((buttonName, index) => <button key={buttonName} className={`button-${index + 1} button-style`}>{buttonName}</button>)}
+        </div>
       </div>
-      <span className='text-author'>Designed and Coded By Yaroslav skiba</span>
-    </div>
+      <p className='text-author'>Designed and Coded By Yaroslav skiba</p>
+    </>
   );
 }
 
